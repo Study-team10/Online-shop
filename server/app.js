49c85c873406cc3a14d443dbb5d7271a714302ec
Olivera <oliveram1996@gmail.com>
2020-03-23 17:47:08 +0100

    cors error fixed

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import userRoutes from "./routes/user";
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/user", userRoutes);
+
+app.use("/", () => {
+  res.json({ "message": "Not found" });
+});
 
 mongoose
   .connect(`mongodb://localhost:27017/shop`, {

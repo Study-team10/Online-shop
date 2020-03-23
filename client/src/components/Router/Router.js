9f0912e8557c4bfcb98f0abc78b22fbaf21e0216
Kristina2103 <kristinapejcic1994@gmail.com>
2020-03-22 20:13:02 -0700

    login & signup initial setup

---
+++ client/src/components/Router/Router.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { publicRoutes } from "routes";
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {publicRoutes.map((route, index) => (
+          <Route path="/">
+            <route.component />
+          </Route>
+        ))}
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ client/src/pages/home.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const home = () => {
+  return <div>Here goes login page</div>;
+};
+export default home;
